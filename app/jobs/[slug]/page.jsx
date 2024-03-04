65b6118c9db395e3ea7ecee4127a7b4161c9762e
hanuman <hanuman.multiqos@gmail.com>
2024-03-04 19:10:12 +0530

    dff

--- app/jobs/[slug]/page.jsx
+++ app/jobs/[slug]/page.jsx
@@ -1,19 +1,19 @@
 import { getJob } from "@/app/api/joblisting/route";
 import React from "react";
 
-// export async function generateStaticParams() {
-//   const resss = await fetch(`${process.env.NEXT_PUBLIC_BACKEND_URL}job-list`, {
-//     method: "POST",
-//     headers: {
-//       "content-type": "application/json",
-//     },
-//     // crossdomain: true
-//   }).then((rr) => rr.json());
+export async function generateStaticParams() {
+  const resss = await fetch(`${process.env.NEXT_PUBLIC_BACKEND_URL}job-list`, {
+    method: "POST",
+    headers: {
+      "content-type": "application/json",
+    },
+    // crossdomain: true
+  }).then((rr) => rr.json());
 
-//   return resss?.data?.jobs?.map((job) => ({
-//     slug: job.slug,
-//   }));
-// }
+  return resss?.data?.jobs?.map((job) => ({
+    slug: job.slug,
+  }));
+}
 
 const JobDetail = async ({ params }) => {
   const response = await getJob(params?.slug);
